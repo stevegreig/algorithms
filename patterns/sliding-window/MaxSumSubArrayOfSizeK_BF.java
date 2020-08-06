7713849374e206ad27ccf8fb8aef155948668244
Steve Greig <steve@stevegreig.io>
2020-08-06 16:17:47 -0400

    Update MaxSumSubArrayOfSizeK_BF.java

--- patterns/sliding-window/MaxSumSubArrayOfSizeK_BF.java
+++ patterns/sliding-window/MaxSumSubArrayOfSizeK_BF.java
@@ -10,6 +10,7 @@ public class MaxSumSubArrayOfSizeK_BF {
 
     public static int findMaxSumSubArray(int k, int[] arr) {
         int maxSum = 0, windowSum;
+        
         for (int i = 0; i <= arr.length - k; i++) {
             windowSum = 0;
             for (int j = i; j < i + k; j++) {
